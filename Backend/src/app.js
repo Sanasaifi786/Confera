5692329f0a98942965d1fa494c502af0f4233e75
Sanasaifi786 <sanasaifi1420@gmail.com>
2026-05-31 23:59:38 +0530

    implement socket connection and user routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,18 +1,19 @@
 import express from "express";
 import {createServer} from "node:http";
 import {Server} from "socket.io";
+import {connectToSocket} from "./controllers/socketManager.js";
 import mongoose from "mongoose";
 import cors from "cors";
 import express from "express";
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = connectToSocket(server);
 
-
-app.get("/home",(req,res)=>{
-    return res.json({"hello":"World"});
-})
+app.set("port", process.env.PORT || 8000);
+app.use(cors());
+app.use(express.json({limit:"40kb"}));
+app.use(express.urlencoded({limit:"40kb",extended:true}));
 
 const start = async()=>{
     app.listen(8000,()=>{
