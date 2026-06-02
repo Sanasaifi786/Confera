0568545ad6fe6e32fe3767992ef2f54f714beed1
Sanasaifi786 <sanasaifi1420@gmail.com>
2026-06-02 23:56:31 +0530

    add dotenv for environment variable management and configure server port

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,7 @@ import {createServer} from "node:http";
 import {Server} from "socket.io";
 import {connectToSocket} from "./controllers/socketManager.js";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 import cors from "cors";
 import express from "express";
 import userRoute from "./routes/userRoute.js";
@@ -11,17 +12,26 @@ const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
 
-app.set("port", process.env.PORT || 8000);
+dotenv.config({
+    path:"./.env"
+});
+// app.set("port", process.env.PORT || 8000);
 app.use(cors());
 app.use(express.json({limit:"40kb"}));
 app.use(express.urlencoded({limit:"40kb",extended:true}));
 
 app.use("/api/v1/user",userRoute);
 
-const start = async()=>{
-    app.listen(8000,()=>{
-        console.log("Server is running on port 8000");
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`🚀 Server is running on port ${process.env.PORT}`);
+        });
     })
-}
+    .catch((error) => {
+        console.error("❌ Server failed to start:", error.message);
+        process.exit(1);
+    });
+
 
 start();
