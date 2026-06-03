8c0bfd9ec697d9faad7b798657e3310a1bcc50db
Sanasaifi786 <sanasaifi1420@gmail.com>
2026-06-03 20:34:35 +0530

    feat: initialize frontend with React, Vite, and ESLint
    - Added package.json for project dependencies and scripts
    - Created App.css for global styles
    - Implemented App component with routing
    - Added index.css for root styles and theming
    - Set up main entry point in main.jsx
    - Developed LandingPage component for the landing page
    - Configured Vite for the development environment

---
+++ frontend/src/pages/landing.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.css'
+function landing() {
+  return (
+    <div className='landingPageContainer'>
+      <nav>
+        <div className="navHeader">
+            <h2>Confera</h2>
+        </div>
+        <div className="navList">
+            <p>Join as Guest</p>
+            <p>Register</p>
+            <div role = 'button'>
+                <p>Login</p>
+            </div>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default landing
