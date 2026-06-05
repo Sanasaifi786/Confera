3ab6d92c38412ec05b068e816944065c50614eed
Sanasaifi786 <sanasaifi1420@gmail.com>
2026-06-05 23:48:57 +0530

    Fix import path for user controller in userRoute.js

--- Backend/src/routes/userRoute.js
+++ Backend/src/routes/userRoute.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import {login,register} from "../controllers/userController.js";
+import {login,register} from "../controllers/user.controller.js";
 const router = Router();
 
 router.route("/login").post(login);
