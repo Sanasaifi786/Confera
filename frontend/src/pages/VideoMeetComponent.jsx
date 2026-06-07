7ba3d31a1772cb0191421abd651f543e432e3252
Sanasaifi786 <sanasaifi1420@gmail.com>
2026-06-07 23:52:58 +0530

    Add VideoMeetComponent for video meeting functionality

---
+++ frontend/src/pages/VideoMeetComponent.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const server_url  = "http://localhost:8000";
+
+var connections = {};
+
+const peerCongifConnections = {
+
+}
+
+function VideoMeetComponent() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default VideoMeetComponent;
+
+//STUN - STUN server are lightweight servers running on the public internet which return the IP address of the requesters device
