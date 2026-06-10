fc472543e8a729ce48279155c76b410729552622
Sanasaifi786 <sanasaifi1420@gmail.com>
2026-06-10 23:20:02 +0530

    Add state management and video reference hooks in VideoMeetComponent

--- frontend/src/pages/VideoMeetComponent.jsx
+++ frontend/src/pages/VideoMeetComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 
 const server_url  = "http://localhost:8000";
 
@@ -14,6 +14,24 @@ function VideoMeetComponent() {
 
     var socketRef = useRef();
     let socketIdRef = useRef();
+
+    let localVideoRef = useRef();
+    let [videoAvailable, setVideoAvailable] = useState(true);
+    let [audioAvailable, setAudioAvailable] = useState(true);
+    let [video, setVideo] = useState();
+    let [audio, setAudio] = useState();
+    let [screen, setScreen] = useState();
+    let [showModal, setShowModal] = useState();
+    let [screenAvaliable, setScreenAvailable] = useState();
+    let [messages, setMessages] = useState([]);
+    let [message, setMessage] = useState("");
+    let [newMessage, setNewMessage] = useState(0);
+    let [askForUsername, setAskForUsername] = useState(true);
+
+    const videoRef = useRef();
+     let [videos, setVideos] = useState([]);
+
+     
 
   return (
     <div>
