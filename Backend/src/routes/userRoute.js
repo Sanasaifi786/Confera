92295bd5c8e95df6243e32ce92160405ab5507ad
Sanasaifi786 <sanasaifi1420@gmail.com>
2026-06-17 12:53:42 +0530

    feat: implement user meeting routes and landing page UI with history management

--- Backend/src/routes/userRoute.js
+++ Backend/src/routes/userRoute.js
@@ -1,10 +1,11 @@
-import {Router} from "express";
-import {login,register} from "../controllers/user.controller.js";
+import { Router } from "express";
+import { login, register, addToActivity, getAllActivities } from "../controllers/user.controller.js";
+
 const router = Router();
 
 router.route("/login").post(login);
 router.route("/register").post(register);
-router.route("/add_to_activity");
-router.route("/get_all_activities");
+router.route("/add_to_activity").post(addToActivity);
+router.route("/get_all_activities").get(getAllActivities);
 
 export default router;
